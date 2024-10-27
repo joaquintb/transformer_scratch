ffb1b71939fa7c7cb5cbbeff0301ef73ba4fe070
Joaquin Torres <joaquintobrw@gmail.com>
2024-10-27 14:01:02 +0100

    es-fr translation, params adjusted

--- train.py
+++ train.py
@@ -142,6 +142,10 @@ def get_ds(config):
     # It only has the train split, so we divide it overselves
     ds_raw = load_dataset(f"{config['datasource']}", f"{config['lang_src']}-{config['lang_tgt']}", split='train')
 
+    # Take a 33% sample of the full dataset
+    subset_size = int(0.33 * len(ds_raw))
+    ds_raw = ds_raw.select(range(subset_size))
+
     # Build tokenizers
     tokenizer_src = get_or_build_tokenizer(config, ds_raw, config['lang_src'])
     tokenizer_tgt = get_or_build_tokenizer(config, ds_raw, config['lang_tgt'])
